0c0ad1ac4b356651ba01d8338e177312874a874f
Ryan Berckmans <ryanberckmans@gmail.com>
2014-12-01 09:30:55 -0500

    add bootstrap to bower.json, Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -18,5 +18,10 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/JavaScript-MD5/js/md5.js');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
+  destDir: 'assets'
+});
 
 module.exports = app.toTree();
